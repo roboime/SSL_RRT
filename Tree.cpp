d8ab8fbc3b8b036b8b8d6aee12ba79bfe503d3bc
Antonio <antoniospg100@gmail.com>
2020-04-09 02:26:25 -0300

    adicionando metodos de inserir e pesquisar na kdtree

--- Tree.cpp
+++ Tree.cpp
@@ -6,7 +6,8 @@
 #include "Tree.hpp"
 #include "Environment.hpp"
 
-Node::Node(double x = 0, double y = 0, Node* parent = nullptr) : _x(x), _y(y), _parent(parent){}
+Node::Node(double x = 0, double y = 0, Node* parent = nullptr) : _x(x), _y(y), _parent(parent), _vec{x,y}{
+}
 
 Node Node::operator + (const Node& obj){
   Node operation;
@@ -42,27 +43,72 @@ Node Node::makeUnitary(){
 }
 
 
-Tree::Tree(double probGoal) : _kdtree(flann::KDTreeSingleIndexParams()){
+Tree::Tree(Node& root, Node& goal, double probGoal) : _root(root), _goal(goal), _kdtree(flann::KDTreeSingleIndexParams()){
+  //iniciar kdtree com o primeiro Node
+  _kdtree.buildIndex(flann::Matrix<double>(root._vec.data(), 1, 2));
+  //adicionar primeiro node na fila
+  _nodemap[root._vec] = root;
+  //verificar se probGoal é válido
   if (probGoal >=0 && probGoal <= 1) _probGoal = probGoal;
   else _probGoal = 1;
 }
 
-Node Tree::extend(Environment& env, ObstacleGrid& obs, Node& current, Node& target, double step){
-    srand(time(NULL));
-    //gera um valor aleatório para comparar com _probGoal
-    double p = (double(rand() % 100 ) / (100));
-    //Expandir em torno do obj
-    if(p <= _probGoal){
-       Node temp = (current + ((target-current).makeUnitary()).multiplyByConstant(step));
-       temp._parent = &current;
-       if(env.checkWallColision(temp) || obs.checkObstacleColision(current, temp)) return Node(DBL_MAX,DBL_MAX,nullptr);
-       else return temp;
-    }
-    //Exapandir aleatoriamente
-    if(p > _probGoal){
-      Node temp = env.randomState();
-      temp._parent = &current;
-      if(env.checkWallColision(temp) || obs.checkObstacleColision(current, temp)) return Node(DBL_MAX,DBL_MAX,nullptr);
-      else return temp;
-    }
+Node Tree::chooseTarget(Environment& env){
+  srand(time(NULL));
+  //gera um valor aleatório para comparar com _probGoal
+  double p = (double(rand() % 100 ) / (100));
+  if(p <= _probGoal) return _goal;
+  else return env.randomState();
 }
+
+Node Tree::calcNN(Node& target){
+  //querry da pesquisa
+  flann::Matrix<double> query(target._vec.data(), 1, 2);
+  //indices em ordem de menor distância
+  vector<int> i(query.rows);
+  flann::Matrix<int> indices(i.data(), query.rows, 2);
+  //distâncias(não usado ainda)
+  vector<double> d(query.rows);
+  flann::Matrix<double> dists(d.data(), query.rows, 2);
+  //fazer pesquisa
+  _kdtree.knnSearch(query, indices, dists, 1, flann::SearchParams());
+  //retornar Node com coordenadas correspondentes
+  double* point = _kdtree.getPoint(indices[0][0]);
+  vector<double> temp(point, point+2);
+  return _nodemap[temp];
+}
+
+Node Tree::extend(Environment& env, ObstacleGrid& obs, double step){
+  Node target = chooseTarget(env);
+  Node nearest = calcNN(target);
+}
+
+void Tree::addPoints(Node& current){
+  //Adicionar no nodemap
+  _nodemap[current._vec] = current;
+  //Adicionar na kdtree
+  _kdtree.addPoints(flann::Matrix<double>(_nodemap[current._vec]._vec.data(), 1, 2));
+
+}
+
+/*
+int main(){
+  Node start(0,0,nullptr);
+  Node goal(100,100,nullptr);
+  Node p1(3,5,nullptr);
+  Node p2(9,10,nullptr);
+  Node p3(20,50,nullptr);
+  Node p4(50,50,nullptr);
+  Node p5(80,70,nullptr);
+  Node p6(8,1,nullptr);
+  Tree tr(start, goal, 1);
+  tr.addPoints(p1);
+  tr.addPoints(p2);
+  tr.addPoints(p3);
+  tr.addPoints(p4);
+  tr.addPoints(p5);
+  tr.addPoints(p6);
+  Node res = tr.calcNN(goal);
+  cout << res._x << "\n" << res._y;
+}
+*/
--- Tree.hpp
+++ Tree.hpp
@@ -4,16 +4,32 @@
 #include <flann/algorithms/kdtree_single_index.h>
 #include <flann/flann.hpp>
 #include <vector>
+#include <functional>
+#include <unordered_map>
+
+using namespace std;
 
 //Forward declarations
 class Environment;
 class ObstacleGrid;
+
+//Hash-function vector
+struct hash_vector {
+    template <class T>
+    size_t operator()(const vector<T>& p) const
+    {
+        auto hash1 = hash<T>()(p[0]);
+        auto hash2 = hash<T>()(p[1]);
+        return hash1 ^ hash2;
+    }
+};
 
 class Node{
 public:
   double _x;
   double _y;
   Node* _parent;
+  vector<double> _vec;
   Node(double x, double y, Node* parent);
   //Definir operação de soma
   Node operator + (const Node& obj);
@@ -27,15 +43,25 @@ public:
   Node multiplyByConstant(double c) const;
   //Calcular unitário
   Node makeUnitary();
+  //Retornar coordenadas
 };
 
 class Tree{
 private:
     double _probGoal;
+    Node _root;
+    Node _goal;
+    unordered_map<vector<double>, Node, hash_vector> _nodemap;
     flann::Index<flann::L2_Simple<double>> _kdtree;
 
 public:
-  Tree(double probGoal);
+  Tree(Node& root, Node& goal, double probGoal);
   //função para determinar a extensão da árvore
-  Node extend(Environment& env, ObstacleGrid& obs, Node& current, Node& target, double step);
+  Node extend(Environment& env, ObstacleGrid& obs, double step);
+  //função para escolher target
+  Node chooseTarget(Environment& env);
+  //calcular Neighrest Neighbor
+  Node calcNN(Node& current);
+  //adicionar pontos na arvore
+  void addPoints(Node& current);
 };
